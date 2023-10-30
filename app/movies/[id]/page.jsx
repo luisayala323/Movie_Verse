cf62d8ce3e2fa77e048f4f4caf0fd6ead4cd65a0
Luis <luisayala323@gmail.com>
2023-10-30 18:04:55 -0400

    adding similar movies

--- app/movies/[id]/page.jsx
+++ app/movies/[id]/page.jsx
@@ -1,14 +1,15 @@
-import { getMovieDetails } from "@/utils/request"
+import { getMovieDetails, getSimilarMovies } from "@/utils/request"
 
 async function MovieDetailsPage({params}) {
     const movieDetails = await getMovieDetails(params.id)
     const IMAGE_BASE_URL = "https://www.themoviedb.org/t/p/w220_and_h330_face"
+    const similarMovies = await getSimilarMovies(params.id)
 
     return (
         <div className="mx-3 my-4">
             <div className="d-flex align-items-center">
                 <div className="col-3">
-                        <img src={IMAGE_BASE_URL + movieDetails.backdrop_path} alt="" />
+                        <img src={IMAGE_BASE_URL + movieDetails.backdrop_path} alt={movieDetails.title} />
                     </div>
                     <div className="mx-5">
                         <h3>{movieDetails.title}</h3>
@@ -23,6 +24,23 @@ async function MovieDetailsPage({params}) {
                             })}</p>
                         </div>
                         <p>{movieDetails.overview}</p>
+                    </div>
+                </div>
+                {/* Similarl Movies */}
+                <div className="my-3">
+                    <h2>Similar Movies</h2>
+                    <div className="flex-wrap gap-3 d-flex">
+                        {similarMovies.map(movie => {
+                        return(
+                            <div>
+                                <img src={IMAGE_BASE_URL + movie.poster_path}/>
+                                <div className="card-body">
+                                    <h5 className="card-title">{movie.title}</h5>
+                                    
+                                </div>
+                            </div>
+                        )
+                        })}
                     </div>
                 </div>
             </div>
