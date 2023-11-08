507edc2b876cfaa0bf5e0987e5800caffff2176d
Adrianmorales21 <adrianmorales212003@gmail.com>
2023-11-08 00:23:32 -0400

    Search bar fix

--- app/movies/search/page.jsx
+++ app/movies/search/page.jsx
@@ -3,14 +3,14 @@ import SearchResults from '@/app/components/SearchResults';
 import { getMovies } from '@/utils/request';
 import React from 'react'
 
-async function SearchPage({params}) {
-    const searchText = params.query;
-    const movies = await getMovies(searchText);
+async function SearchPage({ searchParams }) {
+  const searchText = searchParams.query;
+  const movies = await getMovies(searchText);
 
   return (
     <SearchResults searchText={searchText} movies={movies}></SearchResults>
-  
-    )
+
+  )
 }
 
 export default SearchPage
