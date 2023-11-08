22bd5b83aa25f5f9cec70147fbe04bc4486cd283
Adrianmorales21 <adrianmorales212003@gmail.com>
2023-11-08 00:23:59 -0400

    header fix

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -9,20 +9,20 @@ function Header() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (searchText){
+    if (searchText) {
       router.push(`/movies/search?query=${searchText}`);
     }
   }
   return (
     <div><nav className="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
-    <div className="container-fluid">
-      <Link className="navbar-brand" href="/">MOVIE VERSE</Link>
-      <form className="d-flex" onChange={e => handleSubmit(e)} role="search">
-        <input onChange={e => setSearchText(e.target.value)} className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </nav></div>
+      <div className="container">
+        <Link className="navbar-brand" href="/">MOVIE VERSE</Link>
+        <form className="d-flex" onChange={e => handleSubmit(e)} role="search">
+          <input onChange={e => setSearchText(e.target.value)} className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+          <button className="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
+    </nav></div>
   )
 }
 
