00e099ab0462d5e8f963fdde526d8a824c480243
JeanBarbosa-hue <jeanmaldy@gmail.com>
2023-11-14 01:38:40 -0400

    seeing if books display

--- app/page.js
+++ app/page.js
@@ -1,18 +1,28 @@
-import Image from 'next/image'
-import { getTrendingMovies } from '@/utils/request'
+
+import Image from 'next/image';
+import { getTrendingMovies, getBooks } from '@/utils/request'; // Import the new getBooks function
 import Card from './components/Card';
 
 export default async function Homepage() {
   const movies = await getTrendingMovies();
+  const books = await getBooks('your query here'); // Replace with your actual search query for books
 
   return (
     <div className='container my-3'>
       <h1>Top trending Movies</h1>
-        <div className="flex-wrap gap-4 d-flex">
-          {movies.map(movie => {
-            return <Card movie={movie}></Card>
-          })}
-        </div>
+      <div className="flex-wrap gap-4 d-flex">
+        {movies.map(movie => (
+          <Card key={movie.id} movie={movie} />
+        ))}
+      </div>
+
+      <h1>Top Books</h1>
+      <div className="flex-wrap gap-4 d-flex">
+        {books.map(book => (
+          // Adjust the Card component to handle book data
+          <Card key={book.id} book={book} type="book" />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
--- utils/request.js
+++ utils/request.js
@@ -1,5 +1,7 @@
 const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+const GOOGLE_BOOKS_API_KEY = process.env.NEXT_PUBLIC_BOOKS_API_KEY;
+const GOOGLE_BOOKS_BASE_URL = process.env.NEXT_PUBLIC_BOOKS_BASE_URL;
 
 //Fetch Trending movies
 export const getTrendingMovies = async () => {
@@ -18,8 +20,22 @@ export const getMovies = async (query) => {
 //Fetch Movie Details for a specific movie
 export const getMovieDetails = async (id) => {
   const res = await fetch(`${BASE_URL}/movie/${id}?api_key=${API_KEY}`);
-  const data = await res.json();
-  return data;
+  if (!res.ok) {
+    throw new Error(`Failed to fetch movie with id ${id}`);
+  }
+
+  const movieData = await res.json();
+  if (!movieData || typeof movieData !== 'object') {
+    throw new Error(`Unexpected response data for movie with id ${id}`);
+  }
+
+  let bookData = null;
+  if (movieData.bookId) {
+    const bookRes = await fetch(`${GOOGLE_BOOKS_BASE_URL}/volumes/${movieData.bookId}?key=${GOOGLE_BOOKS_API_KEY}`);
+    bookData = await bookRes.json();
+  }
+
+  return { movie: movieData, book: bookData };
 }
 
 //Fetch Similar Movies to a given movie ID
@@ -29,3 +45,16 @@ export const getSimilarMovies = async (id) => {
   return data.results;
 }
 
+// Fetch Books based on query
+export const getBooks = async (query) => {
+  const res = await fetch(`${GOOGLE_BOOKS_BASE_URL}/volumes?q=${query}&key=${GOOGLE_BOOKS_API_KEY}`);
+  const data = await res.json();
+  return data.items; // Assuming you want to return the items array from the response
+};
+
+// Fetch Book Details for a specific book
+export const getBookDetails = async (id) => {
+  const res = await fetch(`${GOOGLE_BOOKS_BASE_URL}/volumes/${id}?key=${GOOGLE_BOOKS_API_KEY}`);
+  const data = await res.json();
+  return data;
+};
