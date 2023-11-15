f6dbd33eb0c7e4594573fe14ad75489ceebc52a7
Luis <luisayala323@gmail.com>
2023-11-14 23:59:10 -0400

    adding logo to header

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,6 +1,5 @@
-"use client"
 import Link from 'next/link'
-import { useRouter } from 'next/navigation';
+import { useRouter } from 'next/router';
 import { useState } from "react"
 
 function Header() {
@@ -16,7 +15,9 @@ function Header() {
   return (
     <div><nav className="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
       <div className="container">
-        <Link className="navbar-brand" href="/">MOVIE VERSE</Link>
+        <Link className="navbar-brand" href="/">
+          <img src="/logo MV.png" alt="Logo" width="50" height="50" /> MOVIE VERSE
+        </Link>
         <form className="d-flex" onChange={e => handleSubmit(e)} role="search">
           <input onChange={e => setSearchText(e.target.value)} className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
           <button className="btn btn-outline-success" type="submit">Search</button>
