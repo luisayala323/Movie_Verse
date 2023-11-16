37cbea02302c93f0e2cf0b2bda9cf431a3a0bc64
Adrianmorales21 <adrianmorales212003@gmail.com>
2023-11-15 22:24:34 -0400

    style to the card details page

--- app/movies/[id]/page.jsx
+++ app/movies/[id]/page.jsx
@@ -7,12 +7,12 @@ async function MovieDetailsPage({ params }) {
     const streamingAvailability = await getStreamingAvailability([movieDetails.imdb_id]);
 
     return (
-        <div className="mx-3 my-4">
-            <div className="d-flex align-items-center">
+        <div className="container mx-3 my-4">
+            <div className="row">
                 <div className="col-3">
-                    <img src={IMAGE_BASE_URL + movieDetails.backdrop_path} alt={movieDetails.title} />
+                    <img src={IMAGE_BASE_URL + movieDetails.backdrop_path} alt={movieDetails.title} className="img-fluid" style={{ width: '200px', height: '300px', objectFit: 'cover' }} />
                 </div>
-                <div className="mx-5">
+                <div className="col-9">
                     <h3>{movieDetails.title}</h3>
                     <div className="d-flex">
                         <p className="px-2 py-1 text-white rounded bg-warning me-2">{movieDetails.release_date}</p>
@@ -31,35 +31,38 @@ async function MovieDetailsPage({ params }) {
             </div>
 
             {bookDetails && (
-                <div>
-                    <h4>Related Books:</h4>
-                    <h5>{bookDetails.volumeInfo.title}</h5>
-                    <p>{bookDetails.volumeInfo.authors.join(', ')}</p>
-                    {bookDetails.volumeInfo.imageLinks && bookDetails.volumeInfo.imageLinks.thumbnail && (
-                        <img src={bookDetails.volumeInfo.imageLinks.thumbnail} alt={bookDetails.volumeInfo.title} />
-                    )}
-                    <p>{bookDetails.volumeInfo.description}</p>
+                <div className="row my-4">
+                    <div className="col-3">
+                        {bookDetails.volumeInfo.imageLinks && bookDetails.volumeInfo.imageLinks.thumbnail && (
+                            <img src={bookDetails.volumeInfo.imageLinks.thumbnail} alt={bookDetails.volumeInfo.title} className="img-fluid" style={{ width: '200px', height: '300px', objectFit: 'cover' }} />
+                        )}
+                    </div>
+                    <div className="col-9">
+                        <h4>Related Books:</h4>
+                        <h5>{bookDetails.volumeInfo.title}</h5>
+                        <p>{bookDetails.volumeInfo.authors.join(', ')}</p>
+                        <p>{bookDetails.volumeInfo.description}</p>
+                    </div>
                 </div>
             )}
             {/* Streaming Availability */}
             {streamingAvailability && streamingAvailability[movieDetails.imdb_id] && streamingAvailability[movieDetails.imdb_id].waysToWatch && streamingAvailability[movieDetails.imdb_id].waysToWatch.optionGroups ? (
-                <div>
-                    <h4>Streaming Availability:</h4>
-                    <div>
-                        <h4>Ways to Watch {streamingAvailability[movieDetails.imdb_id].title.title}:</h4>
-                        {streamingAvailability[movieDetails.imdb_id].waysToWatch.optionGroups.map((group, index) => (
-                            <div key={index}>
-                                {group.watchOptions.map((option, index) => (
-                                    <div key={index}>
-                                        <p>{option.primaryText} - {option.secondaryText}</p>
-                                        <a href={option.link.uri}>Watch Here</a>
-                                    </div>
-                                ))}
-                            </div>
-                        ))}
-                        <h4>Reviews:</h4>
-                        <p>User Rating: {streamingAvailability[movieDetails.imdb_id].ratings.rating}</p>
-                        <p>Meta Score: {streamingAvailability[movieDetails.imdb_id].metacritic.metaScore}</p>
+                <div className="row my-4">
+                    <div className="col-12">
+                        <h4>Streaming Availability:</h4>
+                        <div>
+                            <h4>Ways to Watch {streamingAvailability[movieDetails.imdb_id].title.title}:</h4>
+                            {streamingAvailability[movieDetails.imdb_id].waysToWatch.optionGroups.map((group, index) => (
+                                <div key={index}>
+                                    {group.watchOptions.map((option, index) => (
+                                        <div key={index}>
+                                            <p>{option.primaryText} - {option.secondaryText}</p>
+                                            <a href={option.link.uri}>Watch Here</a>
+                                        </div>
+                                    ))}
+                                </div>
+                            ))}
+                        </div>
                     </div>
                 </div>
             ) : null}
@@ -68,16 +71,17 @@ async function MovieDetailsPage({ params }) {
             {similarMovies && similarMovies.length > 0 && (
                 <div>
                     <h4>Similar Movies:</h4>
-                    {similarMovies.map((movie, index) => (
-                        <div key={index}>
-                            <img src={IMAGE_BASE_URL + movie.poster_path} alt={movie.title} />
-                            <p>{movie.title}</p>
-                        </div>
-                    ))}
+                    <div className="row">
+                        {similarMovies.map((movie, index) => (
+                            <div key={index} className="col-3">
+                                <img src={IMAGE_BASE_URL + movie.poster_path} alt={movie.title} className="img-fluid" style={{ width: '200px', height: '300px', objectFit: 'cover' }} />
+                                <p>{movie.title}</p>
+                            </div>
+                        ))}
+                    </div>
                 </div>
             )}
         </div>
     );
 }
-
 export default MovieDetailsPage;
